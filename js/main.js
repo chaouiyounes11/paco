b63ff2d3d92ea4490d2bac19da0738b9630fe038
Younes Chaoui <chaouiyounes11@gmail.com>
2018-04-16 18:21:30 +0200

    update

--- js/main.js
+++ js/main.js
@@ -16,14 +16,39 @@ carousel();
 
 
 
-	$(document).ready(function() {
-		$('.js-scrollTo').on('click', function() { // Au clic sur un élément
-			var page = $(this).attr('href'); // Page cible
-			var speed = 750; // Durée de l'animation (en ms)
-			$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
-			return false;
-		});
-	});
 
 
-  animate();
+
+$(document).ready(function(){
+    $("#leClic").click(function(){
+        $("#formulaire").fadeIn(900);
+        $("#formulaire").css("display", "flex");
+
+    });
+});
+
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
